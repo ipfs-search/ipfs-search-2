52ae11a2262627ef50aae0d1364a65c4f89ef278
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-01-14 17:36:05 +0000

    Cleanup of extractor along similar lines.

--- components/extractor/extractor.go
+++ components/extractor/extractor.go
@@ -5,5 +5,29 @@ import "github.com/ipfs-search/ipfs-search-2/types/resource"
 
 // Interface represents an extractor for (meta)data from resources on the dweb.
 type Interface interface {
-	Start(context.Context, <-chan resource.Resource) (<-chan resource.Data, <-chan resource.ResourceError, error)
+	Extract(context.Context, resource.Resource) (resource.Data, error)
+}
+
+// ExtractChannel indexes a channel of resource data in a blocking way, returning error on failure.
+func ExtractChannel(ctx context.Context, i Interface, resourcec <-chan resource.Resource, datac chan<- resource.Data, errc chan<- error) error {
+	for {
+		select {
+		case <-ctx.Done():
+			// Context closed, return
+			return ctx.Err()
+
+		case r := <-resourcec:
+			data, err := i.Extract(ctx, r)
+
+			if err != nil {
+				errc <- &resource.Error{
+					Err:      err,
+					Resource: &r,
+				}
+			}
+
+			datac <- data
+		}
+	}
+
 }
