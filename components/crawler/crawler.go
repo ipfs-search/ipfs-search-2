c717bf0b80da96f3573d269655ce30199685404e
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-01-14 16:55:22 +0000

    More exploration.

--- components/crawler/crawler.go
+++ components/crawler/crawler.go
@@ -6,26 +6,21 @@ import "github.com/ipfs-search/ipfs-search-2/types/resource"
 import "github.com/ipfs-search/ipfs-search-2/components/extractor"
 import "github.com/ipfs-search/ipfs-search-2/components/indexer"
 
-// Interface represents a crawler for resources.
-type Interface interface {
-	Start(context.Context, <-chan resource.Resource) (<-chan error, error)
-}
-
 // Crawler extracts data from resources and indexes them.
 type Crawler struct {
+	s *sniffer.Interface
 	e *extractor.Interface
 	i *indexer.Interface
 }
 
-// Start asynchronously crawling channel of resources, returning error if starting fails and ResourceError during crawling.
-func (c *Crawler) Start(ctx context.Context, resources <-chan resource.Resource) (<-chan error, error) {
+// Start (blocking) crawling channel of resources, returning error if starting fails and ResourceError during crawling.
+func (c *Crawler) Start(ctx context.Context, resources <-chan resource.Resource, errc chan<- error) error {
 	crawlerContext, cancel := context.WithCancel(ctx)
-	errc = make(chan error, 1)
 
 	// Start the extractor
 	data, extractorErrors, err := e.Start(crawlerContext, resources)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	// Start the indexer
@@ -34,16 +29,17 @@ func (c *Crawler) Start(ctx context.Context, resources <-chan resource.Resource)
 		// Cancel context, stopping extractor
 		cancel()
 
-		return nil, err
+		return err
 	}
 
 	// Start crawling
 	go func() {
 		for {
 			select {
-			case <-ctx.Done():
+			case <-crawlerContext.Done():
 				// Context closed, return
-				return
+				return crawlerContext.Err()
+
 			case err := <-extractorErrors:
 				// Non-fatal extractor error
 				errc <- fmt.Errorf("Extractor error: %w", err)
